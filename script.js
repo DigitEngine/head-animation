cfbb747697331fe35d9a1b6276601efaeb4a1daa
trash gremlin <39778493+DigitEngine@users.noreply.github.com>
2022-09-20 08:57:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -25,8 +25,6 @@ function update()
     //clear();
     time += 1 / 60;
     var x = (Math.sin(time / 4) + 1) * 40;
-    var frame = (Math.floor(time * 2) / 2) % 8;
-    frame = clamp(frame, 0, 2);
     ctx.drawImage(head0, 0, 0);
     //if(frame == 0)ctx.drawImage(head0, 0, x);
     //if(frame == 1)ctx.drawImage(head1, 0, x);
