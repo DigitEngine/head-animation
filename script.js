7ed8af654c22740f65dabe5ed4d7b1b9fd4dd41c
trash gremlin <39778493+DigitEngine@users.noreply.github.com>
2022-09-20 08:53:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,14 +22,15 @@ function clamp(x, y, z)
 
 function update()
 {
-    clear();
+    //clear();
     time += 1 / 60;
     var x = (Math.sin(time / 4) + 1) * 40;
     var frame = (Math.floor(time * 2) / 2) % 8;
     frame = clamp(frame, 0, 2);
-    if(frame == 0)ctx.drawImage(head0, 0, x);
-    if(frame == 1)ctx.drawImage(head1, 0, x);
-    if(frame == 2)ctx.drawImage(head2, 0, x);
+    ctx.drawImage(head0, 0, x);
+    //if(frame == 0)ctx.drawImage(head0, 0, x);
+    //if(frame == 1)ctx.drawImage(head1, 0, x);
+    //if(frame == 2)ctx.drawImage(head2, 0, x);
 }
 
 setInterval(update, 1 / 60);
